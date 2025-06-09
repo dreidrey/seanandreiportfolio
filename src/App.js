94a6dbf10d34b42bb067354b3f3c75934f2ccbb9
dreidrey <seanandrei700@gmail.com>
2025-06-09 17:57:56 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Coco from './coco.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="Title">
+          SEAN 
+          ANDREI
+        </p>
+        <p className="Subtitle">
+          About Me
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="Header-header">
+        PORTFOLIO test
+      </header>
+    </div>
+  );
+}
+
+export default Header;
